2d6802127d9e21d4f35a572bbdf839133cb38a97
Edil Hudaiberdiev <hudaiberdiev.ed@gmail.com>
2023-10-28 16:09:06 +0600

    get cells

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Cell{
+  id: number;
+  hasItem: boolean;
+  clicked: string;
 }
+
+
+const App = () => {
+
+  const [field, setField] = useState<Cell[]>([]);
+
+  const getFields = () => {
+    const cells = [];
+    for (let i = 0; i < 36; i++) {
+      cells.push({id: i, hasItem: false, clicked: "hidden"});
+    }
+
+    const randonIndex = Math.floor(Math.random() * cells.length);
+    cells[randonIndex].hasItem = true;
+
+    setField(cells);
+
+    console.log(cells);
+  };
+
+
+  if (field.length === 0) {
+    getFields();
+  }
+
+
+};
 
 export default App
